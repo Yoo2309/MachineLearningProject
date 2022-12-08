0790fb548435a42d10e7d576f3800c18eff8f3d8
Yoo2309 <116976024+Yoo2309@users.noreply.github.com>
2022-12-08 15:52:01 +0700

    Upload

---
+++ Home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.markdown("# Machine Learning's Web 🎈")
+st.sidebar.markdown("# Home 🎈")    
+container = st.container(); 
+
+container.subheader('Trần Ngô Bích Du - 20110618 & Đào Thị Thanh Vi - 20110223')
+container.image('images/machineLearning3.png')
