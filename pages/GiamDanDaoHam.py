a6b5cc163758c6c829ab4bf4bf0ef5cc1e4f3afc
vidao1233 <114890306+vidao1233@users.noreply.github.com>
2022-12-10 04:52:13 +0700

    add 3d chart in Bai3&Temp

--- pages/GiamDanDaoHam.py
+++ pages/GiamDanDaoHam.py
@@ -1,6 +1,7 @@
 from sklearn.linear_model import LinearRegression
 import numpy as np
 from matplotlib import pyplot as plt
+import plotly.graph_objects as go
 
 import streamlit as st
 st.markdown("# Giảm dần đạo hàm ❄️")
@@ -157,6 +158,88 @@ elif (app_mode == 'Bai02a'):
     st.write('Sol found by GD: w = ', w1[-1], ',\nafter %d iterations.' %(it1+1))
 elif (app_mode == 'Bai03'):
     st.title('Bài 3')
+    np.random.seed(100)
+    N = 1000
+    X = np.random.rand(N)
+    y = 4 + 3 * X + .5*np.random.randn(N)
+
+    model = LinearRegression()
+    model.fit(X.reshape(-1, 1), y.reshape(-1, 1))
+    w, b = model.coef_[0][0], model.intercept_[0]
+    print('b = %.4f va w = %.4f' % (b, w))
+
+    one = np.ones((X.shape[0],1))
+    Xbar = np.concatenate((one, X.reshape(-1, 1)), axis = 1)
+
+    def grad(w):
+        N = Xbar.shape[0]
+        return 1/N * Xbar.T.dot(Xbar.dot(w) - y)
+
+    def cost(w):
+        N = Xbar.shape[0]
+        return .5/N*np.linalg.norm(y - Xbar.dot(w))**2
+
+    def myGD(w_init, eta):
+        w = [w_init]
+        for it in range(100):
+            w_new = w[-1] - eta*grad(w[-1])
+            if np.linalg.norm(grad(w_new))/len(w_new) < 1e-3:
+                break 
+            w.append(w_new)
+        return (w, it)
+
+    w_init = np.array([0, 0])
+    (w1, it1) = myGD(w_init, 1)
+    print('Sol found by GD: w = ', w1[-1], ',\nafter %d iterations.' %(it1+1))
+    for item in w1:
+        print(item, cost(item))
+
+    print(len(w1))
+
+    A = N/(2*N)
+    B = np.sum(X*X)/(2*N)
+    C = -np.sum(y)/(2*N)
+    D = -np.sum(X*y)/(2*N)
+    E = np.sum(X)/(2*N)
+    F = np.sum(y*y)/(2*N)
+
+    b = np.linspace(0,6,21)
+    w = np.linspace(0,6,21)
+    b, w = np.meshgrid(b, w)
+    z = A*b*b + B*w*w + C*b*2 + D*w*2 + E*b*w*2 + F
+
+    temp = w1[0]
+    bb = temp[0]
+    ww = temp[1]
+    zz = cost(temp) 
+    ax = plt.axes(projection="3d")
+    ax.plot3D(bb, ww, zz, 'ro', markersize = 3)
+
+    temp = w1[1]
+    bb = temp[0]
+    ww = temp[1]
+    zz = cost(temp) 
+    ax.plot3D(bb, ww, zz, 'ro', markersize = 3)
+
+    temp = w1[2]
+    bb = temp[0]
+    ww = temp[1]
+    zz = cost(temp) 
+    ax.plot3D(bb, ww, zz, 'ro', markersize = 3)
+
+    temp = w1[3]
+    bb = temp[0]
+    ww = temp[1]
+    zz = cost(temp) 
+    ax.plot3D(bb, ww, zz, 'ro', markersize = 3)
+        
+    # Create an object for graph layout
+    data = go.Surface(x = b, y = w, z = z)
+    fig = go.Figure(data)
+
+    st.plotly_chart(fig)
+    
+
 elif (app_mode == 'Bai04'):
     st.title('Bài 4')
     x = np.linspace(-2, 2, 21)
@@ -235,3 +318,14 @@ elif (app_mode == 'Bai05'):
     st.pyplot(fig)
 else:
     st.title('Temp')
+
+    X = np.linspace(-2, 2, 21)
+    Y = np.linspace(-2, 2, 21)
+    X, Y = np.meshgrid(X, Y)
+    Z = X*X + Y*Y
+
+    # Create an object for graph layout
+    data = go.Surface(x = X, y = Y, z = Z)
+    fig = go.Figure(data)
+
+    st.plotly_chart(fig)
