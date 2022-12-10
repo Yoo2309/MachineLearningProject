6df7f20f54fda993b7a191d356f20d8e8a11d34e
Yoo2309 <116976024+Yoo2309@users.noreply.github.com>
2022-12-10 18:07:40 +0700

    Update KNN.py

--- pages/KNN.py
+++ pages/KNN.py
@@ -5,7 +5,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from sklearn import datasets
-from PIL import ImageTk, Image
 import matplotlib.pyplot as plt
 import numpy as np
 from skimage import exposure
